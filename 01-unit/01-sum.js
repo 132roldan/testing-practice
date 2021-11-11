8a9de36953f0fd15bfcf49822386a0b06d434af4
Romualdo <132roldan@gmail.com>
2021-11-10 21:17:33 -0300

    third case

--- 01-unit/01-sum.js
+++ 01-unit/01-sum.js
@@ -1,7 +1,8 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function sum() {
+function sum(a = 0, b = 0) {
+	return a+b;
 
 }
 
@@ -20,11 +21,17 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should ignore additional numbers.
+	//  var result = sum(1, 1, 1);
+  if (result !== 2) throw new Error('Expected sum(1, 1, 1)  to be 2. Received: ' + result);
+
 
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
+	 var result = sum(1);
+  if (result !== 1) throw new Error('Expected sum(1) to be 1. Received: ' + result);
+
 
   // --------------------------------------------------
   // Test Case 4
