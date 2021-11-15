dfd6bf9ad98e492a9797e29a8ccce0273a4077d6
Romualdo <132roldan@gmail.com>
2021-11-15 02:11:10 -0300

    case02-integration passed

--- 02-integration/01-calc.js
+++ 02-integration/01-calc.js
@@ -1,29 +1,33 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function sum(a = 0,b = 0){
-return a+b;
-}	
-function difference(a = 0,b = 0) {
- return a-b;
+function sum(a = 0, b = 0) {
+  return a + b;
+}
+function difference(a = 0, b = 0) {
+  return a - b;
 }
 function product(a = 0, b = 1) {
-	return a*b;
+  return a * b;
 }
-function quotient(a =0, b = 1) {
-	if (b == 0){
-	return console.log('error');
-	}else {
-	return a/b;
-	}
+function quotient(a = 0, b = 1) {
+  if (b == 0) {
+    return console.log("error");
+  } else {
+    return a / b;
+  }
 }
-function calc(op,a,b) {
-	switch (op) {
-		case 'add': return sum(a,b);
-		case 'subtract': return difference(a,b);
-		case 'multiply': return product(a,b);
-		case 'divide' : return quotient(a,b);	
-	}
+function calc(op, a, b) {
+  switch (op) {
+    case "add":
+      return sum(a, b);
+    case "subtract":
+      return difference(a, b);
+    case "multiply":
+      return product(a, b);
+    case "divide":
+      return quotient(a, b);
+  }
 }
 
 // ==================================================
@@ -34,13 +38,19 @@ try {
   // Test Case 1
   // --------------------------------------------------
   // It should return the correct sum when the user provides: 'add', 1, 1.
-  var result = calc('add', 1, 1);
-  if (result !== 2) throw new Error('Expected calc("add", 1, 1) to be 2. Received: ' + result);
+  var result = calc("add", 1, 1);
+  if (result !== 2)
+    throw new Error('Expected calc("add", 1, 1) to be 2. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 2
   // --------------------------------------------------
   // It should return the correct difference when the user provides: 'subtract', 20, 10.
+  var result = calc("subtract", 20, 10);
+  if (result !== 10)
+    throw new Error(
+      'Expected calc("subtract", 20, 10) to be 10. Received: ' + result
+    );
 
   // --------------------------------------------------
   // Test Case 3
@@ -57,12 +67,12 @@ try {
   // --------------------------------------------------
   // It should return the message 'Operation not supported.' when the user provides: exponent, 2, 8.
 
-  console.log('All tests passed successfully.');
+  console.log("All tests passed successfully.");
 
-// ==================================================
-// PRINT ERRORS
-// ==================================================
+  // ==================================================
+  // PRINT ERRORS
+  // ==================================================
 } catch (e) {
-  console.warn('Whoops, the following test did not pass:');
+  console.warn("Whoops, the following test did not pass:");
   console.error(e.message);
 }
